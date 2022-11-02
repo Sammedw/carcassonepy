fec429c1d5241e24d78bc367e51ab3ac208787f9
Sam Medwell <sam_medwell@hotmail.co.uk>
2022-11-02 19:05:35 +0000

    Started interface

---
+++ interface.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# create window
+window = tk.Tk()
+#window.geometry(f"{window.winfo_screenwidth()}x{window.winfo_screenheight()}")
+window.state('zoomed') 
+window.title("Carcassonnepy")
+
+window.mainloop()
