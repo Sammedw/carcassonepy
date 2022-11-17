592157534aaf54708095c5e3acdc475db2e38690
Sam Medwell <sam_medwell@hotmail.co.uk>
2022-11-17 16:06:04 +0000

    chance and choice node

--- agents/UCTagent.py
+++ agents/UCTagent.py
@@ -6,12 +6,8 @@ from agents.baseagent import BaseAgent
 from game import Game
 from tile import Tile
 
-class ChanceNode(Tree):
-    
-    def __init__(self):
-        super().__init__(self)
 
-class Tree:
+class ChoiceNode:
 
     def __init__(self, state: Game, next_tile: Tile, incoming_action: Optional[Action]):
         # current game state object
@@ -20,14 +16,14 @@ class Tree:
         # action taken to reach the current state
         self.incoming_action = incoming_action
         # list of explored children
-        self.children: list[Tree] = []
+        self.children: list[ChanceNode] = []
         # visit and total sums used to calculate UCT
         self.visit_rate = 0
         self.total_reward = 0
         # list of actions from given state that have not been explored
         self.expandable_actions: list[Action] = self.state.get_valid_actions(next_tile)
 
-    def add_child(self, child: Tree):
+    def add_child(self, child: ChanceNode):
         self.children.append(child)
 
     def is_leaf(self):
@@ -37,6 +33,20 @@ class Tree:
         return len(self.expandable_actions) > 0
 
 
+class ChanceNode:
+    
+    def __init__(self, current_deck: list[Tile]):
+        # available tiles to take from current state
+        self.current_deck = current_deck
+        # list of explored children
+        self.children: dict[Tile, ChoiceNode] = {}
+        # visit and total sums used to calculate UCT
+        self.visit_rate = 0
+        self.total_reward = 0
+
+    def select_random_child(self):
+        pass
+
 
 
 class UCTAgent(BaseAgent):
@@ -44,15 +54,15 @@ class UCTAgent(BaseAgent):
     def __init__(self, player_num: int, game: Game):
         super().__init__(player_num, game)
 
-    def expand(self, root: Tree):
+    def expand(self, root: ChoiceNode):
         # choose untried action from current state
         untried_action = random.choice(root.expandable_actions)
         # create new game state after applying action
         new_state = copy.deepcopy(root.state)
         new_state.make_action(untried_action)
-        new_child = Tree()
+        new_child = ChoiceNode()
 
-    def tree_policy(self, root: Tree):
+    def tree_policy(self, root: ChoiceNode):
         current_node = root
         # loop while current node is non terminal
         while not root.is_leaf():
@@ -63,7 +73,7 @@ class UCTAgent(BaseAgent):
 
     def uct_search(self, start_state: Game, incoming_action: Action, next_tile: Tile, iterations: int):
         # create root node
-        root = Tree(copy.deepcopy(start_state), incoming_action, next_tile)
+        root = ChoiceNode(copy.deepcopy(start_state), next_tile, incoming_action)
         # simulate while within computational budget
         for i in range(iterations):
             # select node to expand using tree policy
