d27b4bc4fd8a8fd61a8944e68b7757a9fdd11e6d
Sam <sam_medwell@hotmail.co.uk>
2022-11-21 11:46:20 +0000

    test times

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 
 from itertools import cycle
+import time
 from CFRagent import CFRAgent
 from UCTagent import UCTAgent
 from randomagent import RandomAgent
@@ -7,14 +8,37 @@ from game import Game
 
 
 game = Game(2)
-#players = [RandomAgent(0, game), RandomAgent(1, game)] #UCTAgent(0, game)
-players = [RandomAgent(0, game), UCTAgent(1, game)]
+players = [RandomAgent(0, game), RandomAgent(1, game)] #UCTAgent(0, game)
+#players = [RandomAgent(0, game), UCTAgent(1, game)]
 #players = [CFRAgent(0, game), RandomAgent(1, game)]
 
-scores = [0,0]
+# scores = [0,0]
 
-for g in range(10000):
-    player_cycle = cycle(players)
+# for g in range(10000):
+#     player_cycle = cycle(players)
+#     while(not game.is_game_over()):
+#         next_tile = game.deck.peak_next_tile()
+#         # check for any valid moves
+#         if (len(game.get_valid_actions(next_tile)) == 0):
+#             continue
+#         next(player_cycle).make_move(next_tile)
+
+#     game.print_game_state()
+#     game_scores = game.compute_final_score()
+#     if game_scores[0] > game_scores[1]:
+#         scores[0] += 1
+#     elif game_scores[0] < game_scores[1]:
+#         scores[1] += 1
+
+#     print(f"GAMES: {scores}") 
+#     game.reset()
+
+
+time_sum = 0 
+
+for g in range(100):
+    start = time.time() 
+    player_cycle = cycle(players) 
     while(not game.is_game_over()):
         next_tile = game.deck.peak_next_tile()
         # check for any valid moves
@@ -23,11 +47,9 @@ for g in range(10000):
         next(player_cycle).make_move(next_tile)
 
     game.print_game_state()
-    game_scores = game.compute_final_score()
-    if game_scores[0] > game_scores[1]:
-        scores[0] += 1
-    elif game_scores[0] < game_scores[1]:
-        scores[1] += 1
-
-    print(f"GAMES: {scores}") 
     game.reset()
+    duration = time.time() - start
+    time_sum += duration
+    print("GAME DURATION: " + str(duration) + " seconds")
+
+print("AVERAGE RANDOM GAME DURATION: " + str(time_sum/100) + " seconds")
